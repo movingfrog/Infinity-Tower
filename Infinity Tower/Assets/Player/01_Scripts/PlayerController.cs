dfc0f4fb80e9441d6e7e3d6b3d0a817105f8e556
movingfrog <s24047@gsm.hs.kr>
2025-11-21 14:58:08 +0900

    점프 완성

--- Infinity Tower/Assets/Player/01_Scripts/PlayerController.cs
+++ Infinity Tower/Assets/Player/01_Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Jobs;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
     [Header("플레이어 물리 작용 관련")]
     public float basicMoveSpeed;
-    public float basicJumpForce;
+    public float JumpForce;
+    [Range(0, 2)]
+    public int jumpCount;
+    [Header("점프 판정 관련")]
+    public float groundDistance;
+    public LayerMask groundLayer;
     [Space()]
     Rigidbody2D rigid;
     Vector2 movement;
@@ -16,14 +22,48 @@ public class PlayerController : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        
+    }
+
     private void FixedUpdate()
     {
-        float moveX = movement.x * basicMoveSpeed;
+        movePosition();
+        if (isGrounded() && rigid.linearVelocityY == 0) jumpCount = 2;
+    }
 
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+        return hit.collider != null;
+    }
+    void movePosition()
+    {
+        float moveX = movement.x * basicMoveSpeed;
+        if (movement.x != 0) transform.localScale = new Vector2(movement.x, 1);
+
+        rigid.linearVelocityX = moveX;
     }
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
+    }
+
+    public void OnJump()
+    {
+        if(jumpCount != 0)
+        {
+            rigid.linearVelocityY = 0;
+            rigid.AddForceY(JumpForce, ForceMode2D.Impulse);
+            jumpCount--;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Ray ray = new Ray(transform.position, Vector2.down * groundDistance);
+        Gizmos.DrawRay(ray);
     }
 }
