c9afcfc93d382f0d2720523a836b97e4d44d1c3b
movingfrog <s24047@gsm.hs.kr>
2025-12-10 13:32:41 +0900

    dash animation code

--- Infinity Tower/Assets/Player/01_Scripts/PlayerController.cs
+++ Infinity Tower/Assets/Player/01_Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Jobs;
+using UnityEngine.Rendering;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(PlayerInput))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [Header("플레이어 물리 작용 관련")]
@@ -13,18 +15,20 @@ public class PlayerController : MonoBehaviour
     [Header("점프 판정 관련")]
     public float groundDistance;
     public LayerMask groundLayer;
-    [Space()]
+    [Header("private형식의 접근 변수")]
     Rigidbody2D rigid;
+    Animator ani;
     Vector2 movement;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
@@ -36,21 +40,28 @@ public class PlayerController : MonoBehaviour
     bool isGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundDistance, groundLayer);
+        ani.SetFloat("Velo", rigid.linearVelocityY);
         return hit.collider != null;
     }
     void movePosition()
     {
         float moveX = movement.x * basicMoveSpeed;
-        if (movement.x != 0) transform.localScale = new Vector2(movement.x, 1);
-
+        if (movement.x != 0)
+        {
+            transform.localScale = new Vector2(movement.x, 1);
+        }
         rigid.linearVelocityX = moveX;
     }
+
+    public void AniDashControll()
+    {
+        ani.SetBool("isDash", false);
+    } //애니메이션 이벤트 전용 메서드
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
     }
-
     public void OnJump()
     {
         if(jumpCount != 0)
@@ -60,6 +71,13 @@ public class PlayerController : MonoBehaviour
             jumpCount--;
         }
     }
+    public void OnDash()
+    {
+        ani.SetBool("isDash", true);
+        ani.Play("Dash", 0, 0);
+        rigid.AddForceX(transform.localScale.x * 3, ForceMode2D.Impulse);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
