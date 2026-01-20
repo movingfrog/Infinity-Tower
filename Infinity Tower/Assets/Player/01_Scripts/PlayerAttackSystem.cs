eb7984c6127cf5f72b375ae91c084a761364ae6e
movingfrog <s24047@gsm.hs.kr>
2026-01-20 17:59:52 +0900

    #6
    플레이어 공격 중 원거리 공격을 하기 위해서 플레이어의 공격 시스템을 대폭 변경 그래서 플레이어의 근접 공격을 우선 복구 시킴

--- Infinity Tower/Assets/Player/01_Scripts/PlayerAttackSystem.cs
+++ Infinity Tower/Assets/Player/01_Scripts/PlayerAttackSystem.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,37 +5,18 @@ public enum WeaponType { Sword = 0, Gun = 1, Bow = 2, Pole = 3, None = 4 }
 public class PlayerAttackSystem : MonoBehaviour
 {
     InputSystem_Actions input;
-    Coroutine gunCoroutine;
     Animator PlayerAni;
-    Animator ani;
 
-    [Header("공격 범위")]
-    public Vector2[] attackSize;
-    public Vector2[] attackRange;
-    public LayerMask EnemyLayer;
     [Header("공격 판정")]
-    public float[] attackDamage;
     public float attackDirection;
-    public bool endAttack = true;
     public GameObject WeaponDirection;
     [Header("공격 형태")]
     public GameObject[] weaponPrefeb;
     private WeaponScript weapon;
-    public WeaponType type; //값 저장용
-    public WeaponType WeaponProperty //값 활용 용
-    {
-        get => type;
-        set
-        {
-            type = value;
-            ani.SetBool("isGet", type != WeaponType.None);
-            ani.SetInteger("WeaponCount", (int)type);
-        }
-    }
 
     private void Awake()
     {
-        WeaponDirection.TryGetComponent(out ani);
+        input = new InputSystem_Actions();
         TryGetComponent(out PlayerAni);
         GetWeaponType();
     }
@@ -59,68 +39,20 @@ public class PlayerAttackSystem : MonoBehaviour
     public void OnMove(InputValue value)
     {
         Vector2 movement = value.Get<Vector2>();
-        if (movement.y != 0)
-        {
-            if (movement.y > 0)
-            {
-                WeaponDirection.transform.localScale = new Vector2(-1, 1);
-                WeaponDirection.transform.localPosition = new Vector2(0, attackDirection);
-            }
-            else
-            {
-                WeaponDirection.transform.localScale = new Vector2(1, -1);
-                WeaponDirection.transform.localPosition = new Vector2(0, -attackDirection);
-            }
-        }
-        else
-        {
-            WeaponDirection.transform.localScale = Vector2.one;
-            WeaponDirection.transform.localPosition = new Vector2(attackDirection, 0);
-        }
+        weapon.PositionMove(movement, attackDirection);
     }
 
     private void StartAttack(InputAction.CallbackContext callback)
     {
         if (PlayerAni.GetBool("isUsingSkill") || PlayerAni.GetBool("isDash")) EndAttack(callback);
-        if (ani.GetBool("isGet") && endAttack)
+        if (weapon != null && !weapon.endAttack)
         {
-            endAttack = false;
-            ani.SetTrigger("Attack");
-            InsertDamage(WeaponProperty, callback);
+            weapon.Attack();
         }
     }
     private void EndAttack(InputAction.CallbackContext callback)
     {
-        if (!endAttack) return;
-        if (gunCoroutine != null) StopCoroutine(gunCoroutine);
-        StartCoroutine(AttackCool());
-    }
-    private void InsertDamage(WeaponType weaponType, InputAction.CallbackContext call)
-    {
-        float finalDamage = PlayerStatManager.instance.Damage + weapon.damage;
-        switch (weaponType)
-        {
-            case WeaponType.Sword:
-                Collider2D[] EnemyColl = Physics2D.OverlapBoxAll(transform.position + computeAttackRange(WeaponType.Sword), attackSize[(int)WeaponType.Sword], Sign(transform.localPosition.y) == 0 ? 0 : 90, EnemyLayer);
-                foreach (var enemy in EnemyColl) enemy.GetComponent<IHealth>().Hurt(finalDamage);
-                break;
-            case WeaponType.Gun:
-                gunCoroutine = StartCoroutine(GunAttack(call));
-                break;
-        }
-    }
-    IEnumerator GunAttack(InputAction.CallbackContext callback)
-    {
-        while (weapon.hasAuto && weapon.Shot())
-        {
-            yield return new WaitForSeconds(weapon.AttackCoolTime);
-        }
-        EndAttack(callback);
-    }
-    IEnumerator AttackCool()
-    {
-        yield return new WaitForSeconds(weapon.AttackCoolTime);
-        endAttack = true;
+        weapon.Endattack();
     }
 
     private void GetWeaponType()
@@ -128,28 +60,6 @@ public class PlayerAttackSystem : MonoBehaviour
         if(WeaponDirection.transform.childCount > 0)
         {
             weapon = WeaponDirection.GetComponentInChildren<WeaponScript>();
-            WeaponProperty = weapon.type;
         }
-        else
-        {
-            weapon = Instantiate(weaponPrefeb[(int)WeaponProperty], WeaponDirection.transform).GetComponent<WeaponScript>();
-        }
-    }
-
-    private Vector3 computeAttackRange(WeaponType weaponCode)
-    {
-        Vector3 weaponPosition = WeaponDirection.transform.localPosition;
-        float x = Sign(weaponPosition.x) * attackRange[(int)weaponCode].x * 2;
-        float y = Sign(weaponPosition.y) * attackRange[(int)weaponCode].y * 2;
-        Vector3 position = new Vector3((weaponPosition.x + x) * transform.localScale.x,weaponPosition.y + y, 0);
-        return position;
-    }
-    private float Sign(float value) => value > 0 ? 1f : value < 0 ? -1f : 0;
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue * new Color(1, 1, 1, .1f);
-        Vector2 vec = Sign(transform.localPosition.y) == 0 ? attackSize[(int)WeaponProperty] : new Vector2(attackSize[(int)WeaponProperty].y, attackSize[(int)WeaponProperty].x);
-        Gizmos.DrawCube(transform.position + computeAttackRange(WeaponType.Sword), vec);
     }
 }
--- Infinity Tower/Assets/Player/01_Scripts/WeaponScript.cs
+++ Infinity Tower/Assets/Player/01_Scripts/WeaponScript.cs
@@ -1,13 +1,42 @@
+using NaughtyAttributes;
+using System.Collections;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
+    Coroutine ShotingCoroutine;
+    Animator ani;
+    float Scale;
+
     public WeaponType type;
+    public bool endAttack;
     public float damage;
     public float AttackCoolTime;
+    public LayerMask EnemyLayer;
+    [Foldout("근접 공격")]
+    public Vector2 attackSize;
+    [Foldout("근접 공격")]
+    public Vector2 attackRange;
+    [Foldout("총기 공격 관련")]
     public bool hasAuto;
-    public int currentAmmo { get; private set; }
+    [Foldout("총기 공격 관련")]
     public int MaxAmmo;
+    public int currentAmmo { get; private set; }
+
+    private void Awake()
+    {
+        TryGetComponent(out ani);
+        Scale = Mathf.Abs(transform.localScale.x);
+    }
+
+    private void OnEnable()
+    {
+        ani.SetBool("isGet", true);
+    }
+    private void OnDisable()
+    {
+        ani.SetBool("isGet", false);
+    }
 
     public bool Shot()
     {
@@ -24,5 +53,56 @@ public class WeaponScript : MonoBehaviour
         {
             return true;
         }
+    }
+    public void PositionMove(Vector2 value, float AttackRange)
+    {
+        float ValueX = 1 - Mathf.Abs(Sign(value.y));
+        transform.parent.localPosition = new Vector2(AttackRange * ValueX, AttackRange * Sign(value.y));
+        if (value.y < 0) transform.localScale = Scale * new Vector2(1, -1);
+        else transform.localScale = Scale * new Vector2(ValueX != 0 ? 1 : -1, 1);
+    }
+    public void Attack()
+    {
+        float finalDamage = PlayerStatManager.instance.Damage + damage;
+        endAttack = true;
+        ani.SetTrigger("Attack");
+        switch (type)
+        {
+            case WeaponType.Sword:
+                Collider2D[] EnemyColl = Physics2D.OverlapBoxAll(transform.position + computeAttackRange(), attackSize, Sign(transform.localPosition.y) == 0 ? 0 : 90, EnemyLayer);
+                foreach (var enemy in EnemyColl) enemy.GetComponent<IHealth>().Hurt(finalDamage);
+                break;
+            case WeaponType.Gun:
+
+                break;
+        }
+    }
+
+    public void Endattack()
+    {
+        if (!endAttack) return;
+        if (ShotingCoroutine != null) StopCoroutine(ShotingCoroutine);
+        StartCoroutine(AttackCool());
+    }
+    IEnumerator AttackCool()
+    {
+        yield return new WaitForSeconds(AttackCoolTime);
+        endAttack = false;
+    }
+
+    private Vector3 computeAttackRange() //무기 이미지에 따른 무기 공격 위치의 중심 점 반환
+    {
+        Vector3 weaponPosition = transform.parent.localPosition;
+        float x = Sign(weaponPosition.x) * attackRange.x * 2;
+        float y = Sign(weaponPosition.y) * attackRange.y * 2;
+        Vector3 position = new Vector3((weaponPosition.x + x) * transform.parent.parent.localScale.x, weaponPosition.y + y, 0);
+        return position;
+    }
+    private float Sign(float value) => value > 0 ? 1f : value < 0 ? -1f : 0; //음수,양수,0 반환
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green * new Color(1, 1, 1, .1f);
+        Vector2 vec = Sign(transform.parent.localPosition.y) == 0 ? attackSize : new Vector2(attackSize.y, attackSize.x);
+        Gizmos.DrawCube(transform.parent.parent.position + computeAttackRange(), vec);
     }
 }
