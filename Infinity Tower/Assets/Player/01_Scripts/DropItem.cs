decd6202ee7658b8da003d9eaa08dd3b00e1d86a
movingfrog <s24047@gsm.hs.kr>
2026-03-18 21:42:27 +0900

    인벤토리 중 아이템 획득 만드는 중
    #23
    버그 발견
    - 아이템이 범위 내가 아니어도 획득되는 버그
    - 아이템이 플레이어의 반대로 가는 버그

---
+++ Infinity Tower/Assets/Player/01_Scripts/DropItem.cs
@@ -0,0 +1,29 @@
+using JetBrains.Annotations;
+using UnityEditor.EventSystems;
+using UnityEngine;
+
+public class DropItem : MonoBehaviour
+{
+    public float moveSpeed;
+    public float getSize;
+    public float moveSize;
+    public LayerMask player;
+
+    private void FixedUpdate()
+    {
+        Collider2D outPlayer = Physics2D.OverlapCircle(transform.position, moveSize);
+
+        if(outPlayer != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, outPlayer.transform.position, moveSpeed * Time.deltaTime);
+            if ((transform.position - outPlayer.transform.position).magnitude <= getSize) Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow * new Color(1, 1, 1, .3f);
+        Gizmos.DrawWireSphere(transform.position, moveSize);
+        Gizmos.DrawWireSphere(transform.position, getSize);
+    }
+}
