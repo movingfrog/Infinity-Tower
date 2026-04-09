0fb6fb161362c33e7ebb93729e601063e30754c7
movingfrog <s24047@gsm.hs.kr>
2026-04-09 21:02:10 +0900

    feat: PotionShop에 사용할 Slot스크립트 작성 완료
    #24

--- Infinity Tower/Assets/Map/02_Scripts/PotionShopSystem.cs
+++ Infinity Tower/Assets/Map/02_Scripts/PotionShopSystem.cs
@@ -1,3 +1,27 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PotionShopSystem : MonoBehaviour { }
+public class PotionShopSystem : MonoBehaviour
+{
+    [Header("물약 데이터")]
+    public Potion[] Potions;
+    public PotionSlot[] Slots;
+
+    [Header("UI 속성")]
+    public TextMeshProUGUI PotionName;
+    public TextMeshProUGUI PotionInfo;
+    public Image PotionIcon;
+
+    private void Awake()
+    {
+        for (int i = 0; i < Slots.Length; i++)
+        {
+            Slots[i].SetUp(Potions[i], BuyPotion, ShowInfo);
+        }
+    }
+
+    void ShowInfo(Potion _potion) { }
+
+    void BuyPotion(Potion _potion) { }
+}
--- Infinity Tower/Assets/Map/02_Scripts/PotionSlot.cs
+++ Infinity Tower/Assets/Map/02_Scripts/PotionSlot.cs
@@ -1,16 +1,47 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PotionSlot : MonoBehaviour
+public class PotionSlot : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Potion PotionSO;
+
+    [Header("Slot 加己")]
+    public int a;
+
+    private Action<Potion> LeftClickAction;
+    private Action<Potion> BuyAction;
+
+    [Header("UI 加己")]
+    public Image SlotIcon;
+    public TextMeshProUGUI PotionName;
+    public TextMeshProUGUI PotionPrice;
+
+    private void Start()
     {
-        
+        SlotIcon.sprite = PotionSO.PotionIcon;
+        PotionName.text = PotionSO.potionName;
+        PotionPrice.text = PotionSO.price + "G";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetUp(Potion _potion, Action<Potion> rightAction, Action<Potion> leftAction)
     {
-        
+        PotionSO = _potion;
+        LeftClickAction = leftAction;
+        BuyAction = rightAction;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            LeftClickAction.Invoke(PotionSO);
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            BuyAction.Invoke(PotionSO);
+        }
     }
 }
