93c145772bc0b0b46f90ae615aa62d72867b3c71
movingfrog <s24047@gsm.hs.kr>
2026-06-02 15:12:08 +0900

    feat: 재화 로직 구현 및 Prefab생성
    #40

---
+++ Infinity Tower/Assets/Map/06_ScriptableObject/ChestReward/RewardTable.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RewardTable", menuName = "Scriptable Objects/RewardTable")]
+public class RewardTable : ScriptableObject { }
---
+++ Infinity Tower/Assets/Player/01_Scripts/LootGoods.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class LootGoods : MonoBehaviour
+{
+    [SerializeField]
+    private GoodsType Type;
+
+    [SerializeField]
+    private uint amount;
+
+    [Header("물리 작용")]
+    [SerializeField]
+    private float XForce;
+
+    [SerializeField]
+    private float YForce;
+
+    [SerializeField]
+    private LayerMask Layer;
+
+    private void Awake()
+    {
+        float randXforce = Random.Range(-XForce, XForce);
+        TryGetComponent<Rigidbody2D>(out Rigidbody2D rigid);
+        rigid.AddForce(new Vector2(randXforce, YForce), ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((Layer & (1 << collision.gameObject.layer)) != 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (InventoryManager.Instance != null)
+            InventoryManager.Instance.GetGoods(Type, amount);
+    }
+}
