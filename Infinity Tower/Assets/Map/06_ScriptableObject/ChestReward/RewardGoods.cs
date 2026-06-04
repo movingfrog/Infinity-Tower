9c7f2c7848d030155c53199ffdbcbc2b6e8043db
movingfrog <s24047@gsm.hs.kr>
2026-06-04 12:19:27 +0900

    feat: 상자 보상 SO제작
    #37

---
+++ Infinity Tower/Assets/Map/06_ScriptableObject/ChestReward/RewardGoods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "RewardGoods", menuName = "Scriptable Objects/Reward/RewardGoods")]
+public class RewardGoods : RewardTable
+{
+    [Header("물리 작용")]
+    [SerializeField]
+    private float minXForce;
+
+    [SerializeField]
+    private float maxXForce;
+
+    [SerializeField]
+    private float yForce;
+
+    public override void ExcuteSpawn(GameObject prefabObject, Transform parent)
+    {
+        if (prefabObject == null || parent == null)
+            return;
+
+        Instantiate(prefabObject, parent.position, Quaternion.identity);
+    }
+}
---
+++ Infinity Tower/Assets/Map/06_ScriptableObject/ChestReward/RewardItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RewardItem", menuName = "Scriptable Objects/Reward/RewardItem")]
+public class RewardItem : RewardTable
+{
+    [Header("아이템 생성 속성")]
+    public ItemLevel level { get; set; }
+    public List<Item> items { get; set; }
+    public float commonPercent { get; set; }
+    public float rarePercent { get; set; }
+
+    [Header("물리 작용")]
+    public float xForce;
+
+    public override void ExcuteSpawn(GameObject prefabObject, Transform parent)
+    {
+        if (prefabObject == null || parent == null)
+            return;
+
+        Item item = WorkerHub<ItemCreateWorker>.Instance.CreateItemWorker(
+            items,
+            commonPercent,
+            rarePercent,
+            0
+        );
+        WorkerHub<ItemDropWorker>.Instance.DropItemWork(
+            prefabObject,
+            item,
+            parent.position,
+            DropType.Box,
+            xForce
+        );
+    }
+}
