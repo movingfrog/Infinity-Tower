806633f06d8d51e25f0836971e113a904ee3bd00
movingfrog <s24047@gsm.hs.kr>
2026-06-15 10:31:19 +0900

    docs: Worker부분의 사용방법등을 적었습니다
    #34

--- Infinity Tower/Assets/Managers/Workers.cs
+++ Infinity Tower/Assets/Managers/Workers.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public interface IWorker { }
 
+/// <summary>
+/// Worker구조체를 실행시키기 위한 제네릭 입니다. 필요한 구조체를 할당 후 사용해주시면 됩니다.
+/// </summary>
+/// <typeparam name="T">필요한 Worker구조체를 넣어 메서드를 실행시키면 됩니다</typeparam>
 public static class WorkerHub<T>
     where T : struct, IWorker
 {
@@ -126,13 +130,27 @@ public struct ProbabilityWorker : IWorker
     }
 }
 
+/// <summary>
+/// 이 구조체는 사운드를 실행하기 위한 worker이다
+/// </summary>
 public struct SoundWorker : IWorker
 {
+    /// <summary>
+    /// 이 메서드는 효과음을 실행하기 위한 메서드 입니다
+    /// </summary>
+    /// <param name="source">GameManager에 저장된 AudioSource를 할다하여 주세요</param>
+    /// <param name="SFXClip">현재 필요한 효과음을 GamaManager에서 찾아서 할당아여 주세요</param>
     public void PlaySFX(AudioSource source, AudioClip SFXClip)
     {
         source.PlayOneShot(SFXClip);
     }
 
+    /// <summary>
+    /// 이 메서드는 배경음악을 재생하기 위한 메서드 입니다
+    /// 주의: 한 번 실행한 후 다시 실행하면 이전에 BGM 사라짐
+    /// </summary>
+    /// <param name="source">GameManager에 저장된 AudioSource를 할다하여 주세요</param>
+    /// <param name="BGMClip">현재 스테이지 혹은 보스의 배경음악을 GamaManager에서 찾아서 할당아여 주세요</param>
     public void PlayBGM(AudioSource source, AudioClip BGMClip)
     {
         source.clip = BGMClip;
