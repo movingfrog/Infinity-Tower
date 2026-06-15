e4949f6898e2f5bace956ad0b671e7bc59ff5b86
movingfrog <s24047@gsm.hs.kr>
2026-06-15 19:37:16 +0900

    feat: 맵 보상 획득 시 포탈 생성 시스템 구현
    #51

---
+++ Infinity Tower/Assets/Map/02_Scripts/RoomClearSystem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RoomClearSystem : MonoBehaviour
+{
+    [SerializeField]
+    private MoveRoomPortal InPortal;
+
+    [SerializeField]
+    private MoveRoomPortal OutPortal;
+
+    [Space(10f), SerializeField]
+    private RewardSystem RewardSystem;
+
+    public bool Clear { get; private set; }
+
+    private void OnEnable()
+    {
+        InputManager.Instance.inputActions.Player.Interact.started += OpenChest;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.inputActions.Player.Interact.started -= OpenChest;
+    }
+
+    public void RoomClear()
+    {
+        InPortal.gameObject.SetActive(true);
+        OutPortal.gameObject.SetActive(true);
+    }
+
+    private void OpenChest(InputAction.CallbackContext callback)
+    {
+        if (RewardSystem == null && !Clear)
+        {
+            RoomClear();
+            Clear = true;
+        }
+    }
+}
