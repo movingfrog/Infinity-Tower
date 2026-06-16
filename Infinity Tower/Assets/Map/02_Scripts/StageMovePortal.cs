e4b6d45c3307629fac18aae4fddb1cd52a046a1c
movingfrog <s24047@gsm.hs.kr>
2026-06-17 08:29:10 +0900

    feat: 마지막 맵 생성
    #63

--- Infinity Tower/Assets/Map/02_Scripts/StageMovePortal.cs
+++ Infinity Tower/Assets/Map/02_Scripts/StageMovePortal.cs
@@ -6,8 +6,15 @@ public class StageMovePortal : Portal
     [SerializeField]
     private string StageName;
 
+    [SerializeField]
+    private bool isProto;
+    private static int currentStage;
+
     protected override void TpPlayer(Collider2D player)
     {
+        currentStage++;
+        if (isProto && currentStage >= 2)
+            SceneManager.LoadScene(2);
         SceneManager.LoadScene(StageName);
     }
 }
