75169ef7bf4e00ba35a5655f514d2d8ee6a67a2b
movingfrog <s24047@gsm.hs.kr>
2026-06-16 15:40:04 +0900

    feat: 메인화면 구현
    #53

---
+++ Infinity Tower/Assets/Scenes/MainMenu/PlayBGAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayBGAnimation : MonoBehaviour
+{
+    Animator ani;
+
+    [SerializeField]
+    private float runTime;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        StartCoroutine(AnimationPlay());
+    }
+
+    IEnumerator AnimationPlay()
+    {
+        while (true)
+        {
+            ani.SetBool("Play", true);
+            yield return new WaitForSeconds(runTime / 2);
+            ani.SetBool("Play", false);
+            yield return new WaitForSeconds(runTime / 2);
+        }
+    }
+}
