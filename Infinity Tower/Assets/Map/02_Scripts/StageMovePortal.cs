f4499d4ff60b893443132ed3d5306f3a1d283922
movingfrog <s24047@gsm.hs.kr>
2026-06-16 23:40:54 +0900

    feat: 마지막 상점 및 마지막 포탈 생성
    #63

---
+++ Infinity Tower/Assets/Map/02_Scripts/StageMovePortal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageMovePortal : Portal
+{
+    [SerializeField]
+    private string StageName;
+
+    protected override void TpPlayer(Collider2D player)
+    {
+        SceneManager.LoadScene(StageName);
+    }
+}
