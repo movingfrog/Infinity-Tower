ac121e645fe7bb9951bfa6c6e6e6668dd5357d33
movingfrog <s24047@gsm.hs.kr>
2026-07-03 11:24:13 +0900

    fix: 제미나이 리뷰 수정

---
+++ Infinity Tower/Assets/Enemy/01_Scripts/EndParticle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EndParticle : MonoBehaviour
+{
+    private ParticleSystem particle;
+
+    private void Awake()
+    {
+        particle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (particle != null && !particle.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
