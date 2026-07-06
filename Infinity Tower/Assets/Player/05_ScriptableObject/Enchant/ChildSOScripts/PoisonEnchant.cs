3a58daff6a4c34598cc46df48dfa67ac838a5fa2
movingfrog <s24047@gsm.hs.kr>
2026-07-06 20:00:53 +0900

    feat: 무기 각인 구현
    #74

---
+++ Infinity Tower/Assets/Player/05_ScriptableObject/Enchant/ChildSOScripts/PoisonEnchant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PoisonEnchant", menuName = "WeaponEnchantData/PoisonEnchant")]
+public class PoisonEnchant : WeaponEnchant
+{
+    public override EnchantType Type => EnchantType.Attack;
+
+    private const float PoisonDamage = 2;
+    private const float TickTime = .5f;
+    private const int TickAmount = 3;
+
+    protected override void WeaponUpgrade() => Debug.Log("독 공격 실행");
+
+    public void WeaponUpgrade(IHealth enemy, int weaponLevel, MonoBehaviour runner)
+    {
+        WeaponUpgrade();
+        runner.StartCoroutine(PoisonAttack(weaponLevel * PoisonDamage, enemy));
+    }
+
+    IEnumerator PoisonAttack(float Damage, IHealth Enemy)
+    {
+        for (int i = 0; i < TickAmount; i++)
+        {
+            Enemy.Hurt(Damage);
+            yield return new WaitForSeconds(TickTime);
+        }
+    }
+}
---
+++ Infinity Tower/Assets/Player/05_ScriptableObject/WeaponEnchant.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public enum EnchantType
+{
+    Stat,
+    Attack,
+    Ability,
+}
+
+public abstract class WeaponEnchant : ScriptableObject
+{
+    /// <summary>
+    /// 각인 능력치 상세 설명
+    /// </summary>
+    [TextArea]
+    [field: SerializeField]
+    public string EnchantExplain { get; private set; }
+
+    public abstract EnchantType Type { get; }
+
+    /// <summary>
+    /// 무기 공격 시 실행되어서 추가 능력을 제공하는 메서드
+    /// </summary>
+    protected abstract void WeaponUpgrade();
+}
