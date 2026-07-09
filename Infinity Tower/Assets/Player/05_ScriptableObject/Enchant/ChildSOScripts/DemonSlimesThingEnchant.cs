043375ffbb961e3966eb72a48d490ceb8e45c2a9
movingfrog <s24047@gsm.hs.kr>
2026-07-09 17:46:23 +0900

    feat: 무기 각인 구현
    #77

---
+++ Infinity Tower/Assets/Player/05_ScriptableObject/Enchant/ChildSOScripts/DemonSlimesThingEnchant.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "DemonSlime's_Thing",
+    menuName = "WeaponEnchantData/DemonSlime's_Thing"
+)]
+public class DemonSlimesThingEnchant : WeaponEnchant
+{
+    public override EnchantType Type => EnchantType.Attack;
+
+    private const float TickRate = .15f;
+    private const float DamageValue = .2f;
+    private const int TickAmount = 5;
+
+    public override void WeaponUpgrade(Weapon weapon, GameObject target = null)
+    {
+        float Damage = weapon.damage * DamageValue;
+        float HealAmount = weapon.damage;
+
+        weapon.StartCoroutine(InfernoAttack(Damage, target));
+        PlayerStatManager.instance.ChangeHealth(HealAmount);
+    }
+
+    IEnumerator InfernoAttack(float Damage, GameObject target)
+    {
+        IHealth Enemy = target.GetComponent<IHealth>();
+        if (Enemy == null)
+            yield break;
+        for (int i = 0; i < TickAmount; i++)
+        {
+            Enemy.Hurt(Damage);
+            yield return new WaitForSeconds(TickRate);
+        }
+    }
+}
---
+++ Infinity Tower/Assets/Player/05_ScriptableObject/Enchant/ChildSOScripts/InfernoEnchant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InfernoEnchant", menuName = "WeaponEnchantData/InfernoEnchant")]
+public class InfernoEnchant : WeaponEnchant
+{
+    public override EnchantType Type => EnchantType.Attack;
+
+    private const float InfernoDamage = 3.0f;
+    private const float TickRate = .15f;
+    private const int TickAmount = 3;
+
+    public override void WeaponUpgrade(Weapon weapon, GameObject target = null)
+    {
+        weapon.StartCoroutine(InfernoAttack((int)(weapon.Level + 1) * InfernoDamage, target));
+    }
+
+    IEnumerator InfernoAttack(float Damage, GameObject target)
+    {
+        IHealth Enemy = target.GetComponent<IHealth>();
+        if (Enemy == null)
+            yield break;
+        for (int i = 0; i < TickAmount; i++)
+        {
+            Enemy.Hurt(Damage);
+            yield return new WaitForSeconds(TickRate);
+        }
+    }
+}
