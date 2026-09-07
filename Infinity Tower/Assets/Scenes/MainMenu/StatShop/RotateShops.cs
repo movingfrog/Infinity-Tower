878ba14c281b06ff74ff0911a6b3e75207d9ff08
movingfrog <s24047@gsm.hs.kr>
2026-09-07 21:13:13 +0900

    feat: UI 이미지 위치 회전 코드
    #95

---
+++ Infinity Tower/Assets/Scenes/MainMenu/StatShop/RotateShops.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateShops : MonoBehaviour
+{
+    [Header("UI 속성")]
+    [SerializeField, Tooltip("상점 미리보기 아이콘")]
+    private Image[] PreviewIcons; // 상점 미리보기 아이콘 배열
+
+    [SerializeField, Tooltip("상점 미리보기 이미지")]
+    private Image[] PreviewOption; // 상점 미리보기 옵션 배열
+
+    [SerializeField, Tooltip("상점 미리보기 아이콘 이미지")]
+    private Image[] PreviewIconImages;
+
+    [SerializeField, Tooltip("상점 옵션 이름 텍스트")]
+    private TextMeshProUGUI OptionName; // 상점 옵션 이름 텍스트
+
+    [SerializeField, Tooltip("상점 옵션 설명 텍스트")]
+    private TextMeshProUGUI OptionExplane; // 상점 옵션 설명 텍스트
+
+    [Header("UI 상세 속성")]
+    [SerializeField, Tooltip("선택된 상점 색상")]
+    private Color SelectColor = Color.white;
+
+    [SerializeField, Tooltip("선택되지 않은 상점 색상")]
+    private Color UnSelectColor = Color.white;
+}
