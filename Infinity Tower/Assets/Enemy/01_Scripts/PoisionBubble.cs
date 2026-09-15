1fe95eb3f7678c0dcd83d2a7f02dc71f307177db
movingfrog <s24047@gsm.hs.kr>
2026-09-15 14:12:44 +0900

    fix: 재 수정
    #98

--- Infinity Tower/Assets/Enemy/01_Scripts/PoisionBubble.cs
+++ Infinity Tower/Assets/Enemy/01_Scripts/PoisionBubble.cs
@@ -38,7 +38,7 @@ public class PoisionBubble : MonoBehaviour
         IHealth PlayerHealth = Player.GetComponent<IHealth>();
         for (int i = 0; i < PoisonTickAmount; i++)
         {
-            PlayerHealth.Hurt(AttackDamage, Player.gameObject);
+            PlayerHealth.Hurt(AttackDamage, null);
             yield return new WaitForSeconds(PoisonTickRate);
         }
     }
